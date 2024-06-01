c14c5423306fa1f3f4231fe18bbea2a88160c69a
종성 <eello.bdev@gmail.com>
2024-06-01 11:40:18 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ eello/프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+select
+    usr.user_id,
+    usr.nickname,
+    concat(usr.city, " ", usr.street_address1, " ", usr.street_address2) as 전체주소,
+    concat(substr(usr.tlno, 1, 3), "-", substr(usr.tlno, 4, 4), "-", substr(usr.tlno, 8, 4)) as 전화번호
+from
+    used_goods_board board join used_goods_user usr on board.writer_id = usr.user_id
+group by
+    usr.user_id
+having
+    count(*) >= 3
+order by
+    usr.user_id desc
