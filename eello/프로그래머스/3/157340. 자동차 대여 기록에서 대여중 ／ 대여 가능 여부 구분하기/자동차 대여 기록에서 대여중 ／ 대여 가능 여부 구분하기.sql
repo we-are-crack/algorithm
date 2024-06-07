72ab631892be41ac1463653af37e5dd4a50f514d
종성 <eello.bdev@gmail.com>
2024-06-08 00:36:01 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- eello/프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ eello/프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,22 +1,21 @@
 -- 코드를 입력하세요
 with renting_car as (
-    select car_id, history_id
+    select car_id, 1 as availability
     from car_rental_company_rental_history
     where "2022-10-16" between start_date and end_date
-    group by car_id
-), all_car as (
-    select car_id
-    from car_rental_company_rental_history
     group by car_id
 )
 
 select
-    a.car_id,
+    history.car_id,
     case
-        when rc.history_id is null then "대여 가능"
-        else "대여중"
+        when max(rc.availability) = 1 then "대여중"
+        else "대여 가능"
     end as availability
 from
-    all_car a left outer join renting_car rc using(car_id)
+    car_rental_company_rental_history history
+    left join renting_car rc using(car_id)
+group by
+    history.car_id
 order by
-    a.car_id desc
+    history.car_id desc
