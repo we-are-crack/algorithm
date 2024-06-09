04d6d8672f7c26dd8548d31b768bbcda738f65e7
on1ystar <tjdwls0607@naver.com>
2024-06-09 15:00:55 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.28 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ on1ystar/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for(int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr2[0].length; j++) {
+                int element = 0;
+                for(int k = 0; k < arr1[i].length; k++) {
+                    element += arr1[i][k] * arr2[k][j];
+                }
+                answer[i][j] = element;
+            }
+        }
+        return answer;
+    }
+}
