3e067ee6bf1c17c043d28a8c0d460577dc5a9c63
종성 <eello.bdev@gmail.com>
2024-06-20 11:16:54 +0900

    [level 3] Title: 풍선 터트리기, Time: 252.80 ms, Memory: 211 MB -BaekjoonHub

--- eello/프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ eello/프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,32 +1,27 @@
+import java.util.*;
+
 class Solution {
     
+    /**
+     *  선택한 풍선의 번호가 왼쪽 또는 오른쪽 풍선들의 최소 번호보다 한 번만 작으면 살아남을 수 있는 풍선이됨.
+     *  left 또는 right의 값이 a[i] 또는 a[n - i - 1]의 원소로 갱신되었다는 것은
+     *  왼쪽 또는 오른쪽 풍선들보다 작은 번호이기 때문에 최소 한 번은 작으므로 살아남을 수 있는 풍선.
+     */
+    
     public int solution(int[] a) {
+        Set<Integer> set = new HashSet<>();
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MAX_VALUE;
+        
         int n = a.length;
-        int[] left = new int[n]; // left[i] = 0 ~ i번째까지 원소 중 최솟값
-        int[] right = new int[n]; // right[i] = n ~ n-i번째까지 원소 중 최솟값
-        
-        left[0] = a[0];
-        right[n - 1] = a[n - 1];
-        for (int i = 1; i < n; i++) {
-            left[i] = Math.min(left[i - 1], a[i]);
-            right[n - 1 - i] = Math.min(right[n - i], a[n - 1 - i]);
-        }
-        
-        if (n <= 2) {
-            return n;
-        }
-        
-        int answer = 2;
-        for (int i = 1; i < n - 1; i++) {
-            int l = left[i - 1];
-            int m = a[i];
-            int r = right[i + 1];
+        for (int i = 0; i < n; i++) {
+            left = Math.min(left, a[i]);
+            right = Math.min(right, a[n - i - 1]);
             
-            if (m < l || m < r) {
-                answer++;
-            }
+            set.add(left);
+            set.add(right);
         }
         
-        return answer;
+        return set.size();
     }
 }
