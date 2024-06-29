de965d29f7d4c62492a27d0e387ee269cc13965f
종성 <eello.bdev@gmail.com>
2024-06-29 10:51:36 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- eello/프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ eello/프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,20 +1,34 @@
 -- 코드를 입력하세요
-with jan_sales as (
-    select
-        book.book_id, book.category, book.author_id, author.author_name, book.price * sum(sales.sales) as sales
-    from
-        book
-        join author on book.author_id = author.author_id
-        join book_sales sales on book.book_id = sales.book_id and sales.sales_date like '2022-01%'
-    group by
-        book.book_id, book.category, book.author_id
-)
-
 select
-    author_id, author_name, category, sum(sales) as total_sales
+    author.author_id,
+    author.author_name,
+    book.category,
+    sum(book.price * sales.sales) as total_sales
 from
-    jan_sales
+    book
+    join author using(author_id)
+    join book_sales sales on book.book_id = sales.book_id and sales.sales_date between cast("2022-01-01" as date) and cast("2022-01-31" as date)
 group by
-    author_id, category
+    book.author_id, book.category
 order by
-    author_id, category desc
+    book.author_id, book.category desc
+
+# with jan_sales as (
+#     select
+#         book.book_id, book.category, book.author_id, author.author_name, book.price * sum(sales.sales) as sales
+#     from
+#         book
+#         join author on book.author_id = author.author_id
+#         join book_sales sales on book.book_id = sales.book_id and sales.sales_date like '2022-01%'
+#     group by
+#         book.book_id, book.category, book.author_id
+# )
+
+# select
+#     author_id, author_name, category, sum(sales) as total_sales
+# from
+#     jan_sales
+# group by
+#     author_id, category
+# order by
+#     author_id, category desc
