3ea252d43ee282e6b637f9e37e8d90bbb7b31700
on1ystar <tjdwls0607@naver.com>
2024-07-02 00:09:00 +0900

    [level 2] Title: 소수 찾기, Time: 7.20 ms, Memory: 74.7 MB -BaekjoonHub

--- on1ystar/프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ on1ystar/프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,20 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(String numbers) {
         int answer = 0;
-        ArrayList<Integer> primeNumbers = new ArrayList<>();
-        ArrayList<Integer> permutations = new ArrayList<>();
+        HashSet<Integer> primeNumbers = new HashSet<>();
+        HashSet<Integer> permutations = new HashSet<>();
         for(int i = 1; i <= numbers.length(); i++) {
             permute(new ArrayList<String>(List.of(numbers.split(""))), i, "", permutations);
         }
-        Set<Integer> permutationsSet = new HashSet<>(permutations);
-        System.out.println("permutationsSet = " + permutationsSet);
-        for(int permutation : permutationsSet) {
-            if(isPrime(permutation)) {
-                primeNumbers.add(permutation);
-            }
-        }
-        System.out.println("primeNumbers = " + primeNumbers);
-        return primeNumbers.size();
+        permutations.removeIf(e -> !isPrime(e));
+        return permutations.size();
     }
 
     private boolean isPrime(int number) {
@@ -28,7 +21,7 @@ class Solution {
         return true;
     }
 
-    private void permute(ArrayList<String> numberPieces, int size, String resultNumber, ArrayList<Integer> permutations) {
+    private void permute(ArrayList<String> numberPieces, int size, String resultNumber, HashSet<Integer> permutations) {
         if(size == 0) permutations.add(Integer.parseInt(resultNumber));
         for(int i = 0; i < numberPieces.size(); i++) {
             ArrayList<String> temp = new ArrayList<String>(numberPieces);
