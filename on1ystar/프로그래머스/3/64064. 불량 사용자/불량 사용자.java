11ab42581f0128aa66c88b7d7e55ecdc5e43eedc
on1ystar <tjdwls0607@naver.com>
2024-07-04 20:08:46 +0900

    [level 3] Title: 불량 사용자, Time: 0.30 ms, Memory: 76.7 MB -BaekjoonHub

--- on1ystar/프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ on1ystar/프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    private final HashSet<ArrayList<String>> bannedIds = new HashSet<>();
+    private final HashSet<HashSet<String>> bannedIds = new HashSet<>();
 
     public int solution(String[] userId, String[] bannedId) {
         findBanId(userId, new boolean[userId.length], bannedId, 0, new ArrayList<>());
@@ -10,20 +10,17 @@ class Solution {
     private void findBanId(String[] userId, boolean[] foundUserId,
                            String[] bannedId, int banIndex, ArrayList<String> foundIds) {
         if(banIndex == bannedId.length) {
-            ArrayList<String> foundId = new ArrayList<>(foundIds);
-            foundId.sort(String::compareTo);
+            HashSet<String> foundId = new HashSet<>(foundIds);
             bannedIds.add(foundId);
         }
         else{
             for(int i = 0; i < userId.length; i++){
-                if(!foundUserId[i]){
-                    if(isMatching(userId[i], bannedId[banIndex])) {
-                        foundUserId[i] = true;
-                        foundIds.add(userId[i]);
-                        findBanId(userId, foundUserId, bannedId, banIndex+1, foundIds);
-                        foundUserId[i] = false;
-                        foundIds.remove(foundIds.indexOf(userId[i]));
-                    }
+                if(!foundUserId[i] && isMatching(userId[i], bannedId[banIndex])) {
+                    foundUserId[i] = true;
+                    foundIds.add(userId[i]);
+                    findBanId(userId, foundUserId, bannedId, banIndex+1, foundIds);
+                    foundUserId[i] = false;
+                    foundIds.remove(foundIds.indexOf(userId[i]));
                 }
             }
         }
