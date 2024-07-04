865c4a4f3d111721788849efa52c84264b0ce571
종성 <eello.bdev@gmail.com>
2024-07-04 12:51:25 +0900

    [level 3] Title: 보행자 천국, Time: 110.32 ms, Memory: 114 MB -BaekjoonHub

--- eello/프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ eello/프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -1,10 +1,10 @@
 class Solution {
     
     private static final int MOD = 20170805;
-	private static final int[][] DIR = {{1, 0}, {0, 1}}; // DIR[0] = 진행방향 아래쪽, DIR[1] = 진행방향 오른쪽
-
-	public int solution(int m, int n, int[][] cityMap) {
-		int[][][] dp = new int[m][n][2];
+    private static final int[][] DIR = {{1, 0}, {0, 1}}; // DOWN, RIGHT
+    
+    public int solution(int m, int n, int[][] cityMap) {
+        int[][][] dp = new int[m][n][2];
         dp[0][0][0] = 1;
         
         for (int y = 0; y < m; y++) {
@@ -12,18 +12,19 @@ class Solution {
                 for (int d = 0; d < DIR.length; d++) {
                     int ny = y + DIR[d][0], nx = x + DIR[d][1];
                     
-                    if (m <= ny || n <= nx)
+                    if (m <= ny || n <= nx) {
                         continue;
+                    }
                     
-                    if (cityMap[y][x] == 2) {
-                        dp[ny][nx][d] = dp[y][x][d];
-                    } else if (cityMap[y][x] == 0) {
+                    if (cityMap[y][x] == 0) {
                         dp[ny][nx][d] = (dp[y][x][0] + dp[y][x][1]) % MOD;
+                    } else if (cityMap[y][x] == 2) {
+                        dp[ny][nx][d] = dp[y][x][d];
                     }
                 }
             }
         }
-
-		return (dp[m - 1][n - 1][0] + dp[m - 1][n - 1][1]) % MOD;
-	}
+        
+        return (dp[m - 1][n - 1][0] + dp[m - 1][n - 1][1]) % MOD;
+    }
 }
