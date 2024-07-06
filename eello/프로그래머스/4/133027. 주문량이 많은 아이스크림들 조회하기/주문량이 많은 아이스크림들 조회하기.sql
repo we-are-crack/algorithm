73fab475a384f393d0941b838c6ebdc4ed142c08
종성 <eello.bdev@gmail.com>
2024-07-06 23:16:04 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- eello/프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ eello/프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,11 +1,30 @@
 -- 코드를 입력하세요
 select
-    fh.flavor
+    flavor
 from
-    first_half fh
-    join july on fh.flavor = july.flavor
+    (
+        select flavor, total_order
+        from first_half
+        
+        union
+        
+        select flavor, total_order
+        from july
+    ) a
 group by
-    fh.flavor
+    flavor
 order by
-    fh.total_order + sum(july.total_order) desc
-limit 3
+    sum(total_order) desc
+limit
+    3
+
+# select
+#     fh.flavor
+# from
+#     first_half fh
+#     join july on fh.flavor = july.flavor
+# group by
+#     fh.flavor
+# order by
+#     fh.total_order + sum(july.total_order) desc
+# limit 3
