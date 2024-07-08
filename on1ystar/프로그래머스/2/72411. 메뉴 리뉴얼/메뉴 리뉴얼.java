463be001bd03dbc90922557cca394f60be5c8e7c
on1ystar <tjdwls0607@naver.com>
2024-07-08 21:57:57 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 26.66 ms, Memory: 105 MB -BaekjoonHub

--- on1ystar/프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ on1ystar/프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,57 +1,43 @@
-import java.util.*;
-
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Arrays;
 class Solution {
-
-    ArrayList<String> courseCandidates = new ArrayList<>();
-    int maxCourseCnt = 0;
-
+       static HashMap<String,Integer> map;
+    static int m;
     public String[] solution(String[] orders, int[] course) {
-        ArrayList<String> courseList = new ArrayList<>();
-        HashSet<String> single = new HashSet<>();
-        for (String order : orders) {
-            single.addAll(List.of(order.split("")));
-        }
-        for(int courseSize : course) {
-            HashMap<String, Integer> map = new HashMap<>();
-            combination(new ArrayList<>(single), new boolean[single.size()], courseSize, new StringBuilder());
-            int cnt = 0;
-            for(String courseCandidate : courseCandidates) {
-                for(String order : orders) {
-                    if(order.length() < courseCandidate .length()) continue;
-                    boolean isContain = true;
-                    for(int i = 0; i < courseCandidate.length(); i++) {
-                        if(!(order.contains(courseCandidate.charAt(i) + ""))) {
-                            isContain = false;
-                            break;
-                        }
-                    }
-                    if(isContain) cnt++;
+       PriorityQueue<String> pq = new PriorityQueue<>();
+        for (int i=0;i<course.length;i++){
+            map = new HashMap<>();
+            m=0;
+            for (int j=0;j<orders.length;j++) {
+                find(0, "", course[i], 0, orders[j]);
+            }
+            for (String s : map.keySet()){
+                if (map.get(s)==m&&m>1){
+                    pq.offer(s);
                 }
-                map.put(courseCandidate, cnt);
-                maxCourseCnt = Math.max(maxCourseCnt, cnt);
-                cnt = 0;
             }
-            map.forEach((courseCandidate, count) -> {
-                if(count == maxCourseCnt && maxCourseCnt >= 2) courseList.add(courseCandidate);
-            });
-            maxCourseCnt = 0;
-            courseCandidates.clear();
         }
-        Collections.sort(courseList);
-        return courseList.toArray(new String[0]);
+        String  ans[] = new String[pq.size()];
+        int k=0;
+        while (!pq.isEmpty()){
+            ans[k++] = pq.poll();
+        }
+        return ans;
     }
-
-    private void combination(ArrayList<String> singles, boolean[] visit, int size, StringBuilder comb) {
-        if(size == 0)
-            courseCandidates.add(comb.toString());
-        else {
-            for(int i = 0; i < singles.size(); i++) {
-                if(visit[i]) continue;
-                visit[i] = true;
-                comb.append(singles.get(i));
-                combination(singles, visit.clone(), size - 1, comb);
-                comb.deleteCharAt(comb.length() - 1);
-            }
+    static void find(int cnt,String str,int targetNum,int idx,String word){
+        if (cnt==targetNum){
+            char[] c = str.toCharArray();
+            Arrays.sort(c);
+            String temps="";
+            for (int i=0;i<c.length;i++)temps+=c[i];
+            map.put(temps,map.getOrDefault(temps,0)+1);
+            m = Math.max(m,map.get(temps));
+            return;
+        }
+        for (int i=idx;i<word.length();i++){
+            char now =word.charAt(i);
+            find(cnt+1,str+now,targetNum,i+1,word);
         }
     }
 }
