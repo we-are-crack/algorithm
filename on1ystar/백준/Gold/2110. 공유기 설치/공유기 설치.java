1672b13c09d9f5bdcf714071d4d6c9dddaac7104
on1ystar <tjdwls0607@naver.com>
2024-07-16 21:02:28 +0900

    [Gold IV] Title: 공유기 설치, Time: 1048 ms, Memory: 181120 KB -BaekjoonHub

---
+++ on1ystar/백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int house = scanner.nextInt();
+        int wifi = scanner.nextInt();
+
+        ArrayList<Integer> housePos = new ArrayList<>();
+        for(int i = 0; i < house; i++) {
+            housePos.add(scanner.nextInt());
+        }
+        Collections.sort(housePos);
+        if(wifi == 2) {
+            System.out.println(housePos.get(house - 1) - housePos.get(0));
+            return;
+        }
+        int start = 1;
+        int end = housePos.get(house - 1) / (wifi - 1);
+        int maxDistance = 0;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            int wifiCount = settingWifi(housePos, mid);
+            if(wifiCount >= wifi) {
+                start = mid + 1;
+                maxDistance = mid;
+            }
+            else end = mid - 1;
+        }
+        System.out.println(maxDistance);
+    }
+    static private int settingWifi(ArrayList<Integer> housePos, int distance) {
+        int count = 1;
+        int i = 0;
+        int j = 1;
+        while(j < housePos.size()) {
+            if(housePos.get(j) - housePos.get(i) >= distance) {
+                count++;
+                i = j;
+                j = i + 1;
+                continue;
+            }
+            j++;
+        }
+        return count;
+    }
+}
