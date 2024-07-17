fc0e7779337df3a0f4a176240a3339da40eb9eaa
종성 <eello.bdev@gmail.com>
2024-07-17 11:56:04 +0900

    [level 2] Title: 피보나치 수, Time: 1.70 ms, Memory: 77.6 MB -BaekjoonHub

--- eello/프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ eello/프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -3,13 +3,13 @@ class Solution {
     private static final int MOD = 1_234_567;
     
     public int solution(int n) {
-        int[] fibonacci = new int[n + 1];
-        fibonacci[1] = 1;
-        
+        int pp = 0, p = 1;
         for (int i = 2; i <= n; i++) {
-            fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % MOD;
+            p += pp;
+            pp = p - pp;
+            p %= MOD;
         }
         
-        return fibonacci[n];
+        return p;
     }
 }
