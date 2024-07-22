3ef4e3cbcf1f789cd2921b7ad077bc4577548117
종성 <eello.bdev@gmail.com>
2024-07-22 11:14:22 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 68.2 MB -BaekjoonHub

--- eello/프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ eello/프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,8 +1,6 @@
-import java.util.*;
-
 class Solution {
+    
     public int solution(int[] arr) {
-        Arrays.sort(arr);
         int answer = arr[0];
         for (int i = 1; i < arr.length; i++) {
             answer = lcm(answer, arr[i]);
@@ -11,23 +9,12 @@ class Solution {
         return answer;
     }
     
-    private int lcm(int num1, int num2) {
-        int gcd = gcd(num1, num2);
-        return gcd == -1 ? num1 * num2 : num1 * (num2 / gcd);
+    private int lcm(int a, int b) {
+        return a * b / gcd(a, b);
     }
     
-    private int gcd(int num1, int num2) {
-        int big = num1 > num2 ? num1 : num2;
-        int small = num1 > num2 ? num2 : num1;
-        
-        if (big % small == 0) return small;
-        
-        for (int i = small - 1; i > 1; i--) {
-            if (big % i == 0 && small % i == 0) {
-                return i;
-            }
-        }
-        
-        return -1;
+    private int gcd(int a, int b) {
+        if (a > b) return b == 0 ? a : gcd(b, a % b);
+        else return a == 0 ? b : gcd(a, b % a);
     }
 }
