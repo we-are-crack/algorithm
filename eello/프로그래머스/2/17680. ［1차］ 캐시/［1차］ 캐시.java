a338dcd977544a5f9a3a2da53dedbb262efcf2ad
종성 <eello.bdev@gmail.com>
2024-07-26 23:42:58 +0900

    [level 2] Title: [1차] 캐시, Time: 2.51 ms, Memory: 83.9 MB -BaekjoonHub

--- eello/프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ eello/프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,21 +3,26 @@ import java.util.*;
 class Solution {
     
     public int solution(int cacheSize, String[] cities) {
-        Map<String, Integer> cache = new HashMap<>();
+        if (cacheSize == 0)
+            return cities.length * 5;
+        
+        List<String> cache = new LinkedList<>();
         
         int answer = 0;
         for (int i = 0; i < cities.length; i++) {
             String city = cities[i].toLowerCase();
-            answer += cache.containsKey(city) ? 1 : 5;
             
-            if (cacheSize == 0) continue;
-            
-            cache.put(city, i);
-            if (cache.size() > cacheSize) {
-                List<Map.Entry<String, Integer>> entry = new ArrayList<>(cache.entrySet());
-                Collections.sort(entry, (a, b) -> a.getValue() - b.getValue());
-                cache.remove(entry.get(0).getKey());
+            if (cache.contains(city)) {
+                answer += 1;
+                cache.remove(city);
+            } else {
+                answer += 5;
+                
+                if (cache.size() == cacheSize)
+                    cache.remove(0);
             }
+            
+            cache.add(city);
         }
         
         return answer;
