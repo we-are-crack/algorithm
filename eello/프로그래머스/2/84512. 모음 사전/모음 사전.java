ae820b00a71c8483a882c8b273e195c9aab8246d
종성 <eello.bdev@gmail.com>
2024-08-08 09:56:06 +0900

    [level 2] Title: 모음 사전, Time: 21.47 ms, Memory: 78 MB -BaekjoonHub

---
+++ eello/프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    private static final int MAX_WORD_LENGTH = 5;
+    private int answer, idx;
+    private boolean found;
+    
+    public int solution(String word) {
+        dict(word, "");
+        return answer;
+    }
+    
+    private void dict(String target, String word) {
+        if (target.equals(word)) {
+            answer = idx;
+        }
+        
+        if (answer != 0 || word.length() == MAX_WORD_LENGTH) {
+            return;
+        }
+        
+        char[] vowel = {'A', 'E', 'I', 'O', 'U'};
+        for (char v : vowel) {
+            idx++;
+            dict(target, word + v);
+        }
+    }
+}
