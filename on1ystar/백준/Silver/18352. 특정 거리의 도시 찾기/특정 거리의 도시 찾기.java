a16710fe56b224edd315ca4e1ddcb16f118f406c
on1ystar <tjdwls0607@naver.com>
2024-08-15 23:39:48 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1200 ms, Memory: 339452 KB -BaekjoonHub

---
+++ on1ystar/백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        int cities = Integer.parseInt(line[0]);
+        int roads = Integer.parseInt(line[1]);
+        int shortestPath = Integer.parseInt(line[2]);
+        int start = Integer.parseInt(line[3]);
+
+        PriorityQueue pq = new PriorityQueue();
+        int[] distance = new int[300_001];
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>(300_001);
+        for(int i = 0; i < cities + 1; i++) graph.add(new ArrayList<>());
+        for(int i = 0; i < roads; i++) {
+            String[] road = br.readLine().split(" ");
+            graph.get(Integer.parseInt(road[0])).add(Integer.parseInt(road[1]));
+        }
+        q.offer(start);
+        while(!q.isEmpty()) {
+            int now = q.poll();
+            if(distance[now] > shortestPath) break;
+            for(int next : graph.get(now)) {
+                if(distance[next] == 0 && next != start) {
+                    q.offer(next);
+                    distance[next] = distance[now] + 1;
+                    if(distance[next] == shortestPath) pq.offer(next);
+                }
+            }
+        }
+        if(pq.size() == 0) System.out.println(-1);
+        while(!pq.isEmpty())
+            System.out.println(pq.poll());
+    }
+ }
