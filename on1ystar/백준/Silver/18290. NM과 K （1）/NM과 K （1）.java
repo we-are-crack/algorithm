3724d8882f5c12ff24f77b3ed7ea53cbf7d2d2a1
on1ystar <tjdwls0607@naver.com>
2024-08-17 20:37:08 +0900

    [Silver I] Title: NM과 K (1), Time: 3536 ms, Memory: 302068 KB -BaekjoonHub

--- on1ystar/백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
+++ on1ystar/백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
@@ -7,30 +7,30 @@ public class Main {
     static int n;
     static int m;
     static int k;
+    static String[][] matrix;
+    static boolean[][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] line = br.readLine().split(" ");
         n = Integer.parseInt(line[0]);
         m = Integer.parseInt(line[1]);
         k = Integer.parseInt(line[2]);
-        String[][] matrix = new String[n][m];
+        matrix = new String[n][m];
+        visited = new boolean[n][m];
         for(int i = 0; i < n; i++)
             matrix[i] = br.readLine().split(" ");
 
-        boolean[][] visited = new boolean[n][m];
         ArrayList<int[]> selectedIndex = new ArrayList<>();
-        System.out.println(dfs(matrix, visited, selectedIndex, 0, Integer.MIN_VALUE));
+        System.out.println(dfs(selectedIndex, 0, Integer.MIN_VALUE));
     }
-    private static int dfs(String[][] matrix, boolean[][] visited, ArrayList<int[]> selectedIndex, int sum, int max) {
-        if(selectedIndex.size() == k) {
-            return Math.max(sum, max);
-        }
+    private static int dfs(ArrayList<int[]> selectedIndex, int sum, int max) {
+        if(selectedIndex.size() == k) return Math.max(sum, max);
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(!visited[i][j] && checkNear(selectedIndex, i, j)) {
                     visited[i][j] = true;
                     selectedIndex.add(new int[]{i, j});
-                    max = Math.max(max, dfs(matrix, visited, selectedIndex, sum + Integer.parseInt(matrix[i][j]), max));
+                    max = Math.max(max, dfs(selectedIndex, sum + Integer.parseInt(matrix[i][j]), max));
                     visited[i][j] = false;
                     selectedIndex.remove(selectedIndex.size() - 1);
                 }
