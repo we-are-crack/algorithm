0d880d2b7f533910a97684e2a830999f2e70b7b9
on1ystar <tjdwls0607@naver.com>
2024-08-17 21:10:19 +0900

    [Silver I] Title: NM과 K (1), Time: 868 ms, Memory: 15832 KB -BaekjoonHub

--- on1ystar/백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
+++ on1ystar/백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
@@ -7,46 +7,39 @@ public class Main {
     static int n;
     static int m;
     static int k;
-    static String[][] matrix;
+    static int[][] matrix;
     static boolean[][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] line = br.readLine().split(" ");
-        n = Integer.parseInt(line[0]);
-        m = Integer.parseInt(line[1]);
-        k = Integer.parseInt(line[2]);
-        matrix = new String[n][m];
-        visited = new boolean[n][m];
-        for(int i = 0; i < n; i++)
-            matrix[i] = br.readLine().split(" ");
-
-        ArrayList<int[]> selectedIndex = new ArrayList<>();
-        System.out.println(dfs(selectedIndex, 0, Integer.MIN_VALUE));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        matrix = new int[n + 2][m + 2];
+        visited = new boolean[n + 2][m + 2];
+        for(int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= m; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        System.out.println(dfs(0, Integer.MIN_VALUE, 0));
     }
-    private static int dfs(ArrayList<int[]> selectedIndex, int sum, int max) {
-        if(selectedIndex.size() == k) return Math.max(sum, max);
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(!visited[i][j] && checkNear(selectedIndex, i, j)) {
+    private static int dfs(int sum, int max, int cnt) {
+        if(cnt == k) return Math.max(sum, max);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(!visited[i][j] && isNearbyOk(i, j)) {
                     visited[i][j] = true;
-                    selectedIndex.add(new int[]{i, j});
-                    max = Math.max(max, dfs(selectedIndex, sum + Integer.parseInt(matrix[i][j]), max));
+                    max = Math.max(max, dfs(sum + matrix[i][j], max, cnt + 1));
                     visited[i][j] = false;
-                    selectedIndex.remove(selectedIndex.size() - 1);
                 }
             }
         }
         return max;
     }
-    private static boolean checkNear(ArrayList<int[]> selectedIndex, int i, int j) {
-        for(int[] index : selectedIndex) {
-            if(index[0] + 1 == i && index[1] == j
-                || index[0] - 1 == i && index[1] == j
-                || index[0] == i && index[1] + 1 == j
-                || index[0] == i && index[1] - 1 == j) {
-                return false;
-            }
-        }
+    private static boolean isNearbyOk(int i, int j) {
+        if(visited[i - 1][j] || visited[i][j - 1] || visited[i + 1][j] || visited[i][j + 1]) return false;
         return true;
     }
  }
