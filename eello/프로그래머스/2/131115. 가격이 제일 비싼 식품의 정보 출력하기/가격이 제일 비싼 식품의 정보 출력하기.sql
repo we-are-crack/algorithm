fd672ffc6b489639d5ae1711699995adb6b2efbd
종성 <eello.bdev@gmail.com>
2024-08-22 21:39:34 +0900

    [level 2] Title: 가격이 제일 비싼 식품의 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ eello/프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -0,0 +1,15 @@
+select
+    product_id,
+    product_name,
+    product_cd,
+    category,
+    price
+from
+    food_product
+where
+    price = (
+        select
+            max(price)
+        from
+            food_product
+    )
