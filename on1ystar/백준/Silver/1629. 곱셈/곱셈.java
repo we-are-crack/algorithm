3b1b9f412abcc452f0b3fc618ec8d72c903eb7c6
on1ystar <tjdwls0607@naver.com>
2024-09-18 08:46:27 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

---
+++ on1ystar/백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,29 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        long result = 1;
+
+        while (b >= 1) {
+            if (b % 2 == 1) {
+                result *= a;
+                result %= c;
+            }
+
+            a *= a;
+            a %= c;
+            b /= 2;
+        }
+
+        System.out.println(result);
+    }
+}
