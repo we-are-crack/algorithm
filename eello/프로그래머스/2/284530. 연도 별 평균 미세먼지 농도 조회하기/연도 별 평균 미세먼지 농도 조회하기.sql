375df6565861593622f9142e65f1c82a809bc487
종성 <eello.bdev@gmail.com>
2024-09-18 21:08:29 +0900

    [level 2] Title: 연도 별 평균 미세먼지 농도 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ eello/프로그래머스/2/284530. 연도 별 평균 미세먼지 농도 조회하기/연도 별 평균 미세먼지 농도 조회하기.sql
@@ -0,0 +1,13 @@
+select
+    year(ym) as 'year',
+    round(avg(pm_val1), 2) as 'PM10',
+    round(avg(pm_val2), 2) as 'PM2.5'
+from
+    air_pollution
+where
+    location2 = '수원'
+group by
+    year(ym)
+order by
+    year
+    
