78a2381debef7937ed3a4964612d04937e5259de
종성 <eello.bdev@gmail.com>
2024-09-28 20:48:02 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ eello/프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,13 @@
+select
+    user_id,
+    product_id
+from
+    online_sale
+group by
+    user_id,
+    product_id
+having
+    count(online_sale_id) >= 2
+order by
+    user_id,
+    product_id desc
