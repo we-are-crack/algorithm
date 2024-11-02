2ffb9e524c6ee65b1106c6a0a3cf207135253871
on1ystar <tjdwls0607@naver.com>
2024-11-02 22:05:49 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 228 ms, Memory: 17340 KB -BaekjoonHub

--- on1ystar/백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ on1ystar/백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -20,9 +20,6 @@ public class Main {
             return col;
         }
 
-        public int getMultiCount(Matrix matrix) {
-            return this.row * this.col * matrix.getCol();
-        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,19 +37,15 @@ public class Main {
             return;
         }
 
-        // d[곱한 행렬 개수 + 1][마지막 행렬 위치]
-        // dxy = matrices[y - x] * ... * matrices[y - 1] * matrices[y - 0]
         int[][] d = new int[tc][tc];
         for (int i = 1; i < tc; i++) {
             for (int j = i; j < tc; j++) {
 
             int minCount = Integer.MAX_VALUE;
                 for (int k = 0; k < i; k++) {
-                    Matrix leftMatrix = new Matrix(matrices[j - i].getRow(), matrices[j - i + k].getCol());
-                    Matrix rightMatrix = new Matrix(matrices[j - i + k + 1].getRow(), matrices[j].getCol());
-                    int leftMultiCount = d[k][j - i + k];
-                    int rightMultiCount = d[i - k - 1][j];
-                    minCount = Math.min(minCount, leftMultiCount + rightMultiCount + leftMatrix.getMultiCount(rightMatrix));
+                    minCount = Math.min(minCount, d[k][j - i + k] 
+                            + d[i - k - 1][j] 
+                            + matrices[j - i].getRow() * matrices[j - i + k].getCol() * matrices[j].getCol());
                 }
 
                 d[i][j] = minCount;
