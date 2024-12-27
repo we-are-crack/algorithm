e5bfe7f340f4ddc480c96c79bf326c6fe126d41a
on1ystar <tjdwls0607@naver.com>
2024-12-27 16:59:04 +0900

    [Gold V] Title: 연속합 2, Time: 276 ms, Memory: 27100 KB -BaekjoonHub

--- on1ystar/백준/Gold/13398. 연속합 2/연속합 2.java
+++ on1ystar/백준/Gold/13398. 연속합 2/연속합 2.java
@@ -9,25 +9,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int[] arr = new int[n + 1];
+        
         int[][] d = new int[n + 1][2];
         int answer = MIN_VALUE;
-        arr[0] = MIN_VALUE;
         d[0][0] = MIN_VALUE;
         d[0][1] = MIN_VALUE;
         for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            d[i][0] = Math.max(arr[i] + d[i - 1][0], arr[i]);
-            d[i][1] = Math.max(arr[i] + d[i - 1][1], d[i - 1][0]);
+            int num = Integer.parseInt(st.nextToken());
+            
+            if (d[i - 1][0] > 0) {
+                d[i][0] = num + d[i - 1][0];
+            } else {
+                d[i][0] = num;
+            }
+            
+            d[i][1] = Math.max(num + d[i - 1][1], d[i - 1][0]);
             answer = Math.max(answer, Math.max(d[i][0], d[i][1]));
         }
 
         System.out.println(answer);
-
-//        for (int i = 1; i <= n; i++) {
-//            System.out.println("d[" + i + "] = " + Arrays.toString(d[i]));
-//        }
-
     }
 }
