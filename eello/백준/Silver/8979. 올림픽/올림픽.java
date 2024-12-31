2b10f34a8f4a0c4717791c07b1e8fe5a2060a6c2
종성 <eello.bdev@gmail.com>
2024-12-31 11:01:01 +0900

    [Silver V] Title: 올림픽, Time: 100 ms, Memory: 13964 KB, Score: 8 point -BaekjoonHub

---
+++ eello/백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] score = new int[n + 1][3];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int country = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < 3; j++) {
+                score[country][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[] selected = score[k];
+        int rank = 1;
+        for (int country = 1; country <= n; country++) {
+            if (country == k) {
+                continue;
+            }
+
+            if (compare(selected, score[country]) > 0) {
+                rank++;
+            }
+        }
+
+        System.out.println(rank);
+    }
+
+    private static int compare(int[] c1, int[] c2) {
+        if (c1[0] != c2[0]) {
+            // 금메달 비교
+            return c1[0] - c2[0];
+        }
+
+        if (c1[1] != c2[1]) {
+            // 은메달 비교
+            return c1[1] - c2[1];
+        }
+
+        // 동메달 비교
+        return c1[2] - c2[2];
+    }
+}
