daf301d30a88547109af0844320154592b087849
종성 <eello.bdev@gmail.com>
2025-02-17 12:23:16 +0900

    [Gold II] Title: 로봇 조종하기, Time: 544 ms, Memory: 90352 KB -BaekjoonHub

--- eello/백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
+++ eello/백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -6,13 +6,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         int[][] mars = new int[n][m];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < mars.length; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < mars[i].length; j++) {
                 mars[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -24,21 +25,20 @@ public class Main {
         }
 
         for (int i = 1; i < n; i++) {
-            int[][] temp = new int[2][m];
+            int[] temp1 = new int[m];
+            temp1[0] = dp[i - 1][0] + mars[i][0];
+            for (int j = 1; j < m; j++) {
+                temp1[j] = Math.max(dp[i - 1][j], temp1[j - 1]) + mars[i][j];
+            }
 
-            int left = 0, right = m - 1;
-            temp[0][left] = dp[i - 1][left] + mars[i][left];
-            temp[1][right] = dp[i - 1][right] + mars[i][right];
-            while (left < m - 1) {
-                left++;
-                right--;
-
-                temp[0][left] = Math.max(dp[i - 1][left], temp[0][left - 1]) + mars[i][left];
-                temp[1][right] = Math.max(dp[i - 1][right], temp[1][right + 1]) + mars[i][right];
+            int[] temp2 = new int[m];
+            temp2[m - 1] = dp[i - 1][m - 1] + mars[i][m - 1];
+            for (int j = m - 2; j >= 0; j--) {
+                temp2[j] = Math.max(dp[i - 1][j], temp2[j + 1]) + mars[i][j];
             }
 
             for (int j = 0; j < m; j++) {
-                dp[i][j] = Math.max(temp[0][j], temp[1][j]);
+                dp[i][j] = Math.max(temp1[j], temp2[j]);
             }
         }
 
