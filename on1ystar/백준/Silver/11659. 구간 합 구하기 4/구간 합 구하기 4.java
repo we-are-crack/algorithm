d54f8854ca4fc9fa9e3760ba3bf910750d4512c6
on1ystar <tjdwls0607@naver.com>
2025-03-18 03:41:08 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 568 ms, Memory: 52644 KB -BaekjoonHub

---
+++ on1ystar/백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] d = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            d[i] = Integer.parseInt(st.nextToken()) + d[i - 1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            sb.append(d[end] - d[start - 1]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
