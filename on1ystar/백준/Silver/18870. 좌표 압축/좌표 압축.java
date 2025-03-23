d8f4264d155ba70101fd6ded63fdb24994f80931
on1ystar <tjdwls0607@naver.com>
2025-03-23 15:54:11 +0900

    [Silver II] Title: 좌표 압축, Time: 1652 ms, Memory: 179116 KB -BaekjoonHub

--- on1ystar/백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ on1ystar/백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -11,27 +11,40 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
-        int[][] compArr = new int[n][2];
+        int[] sortingArr = new int[n];
+
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            compArr[i] = new int[]{arr[i], i};
+            sortingArr[i] = arr[i];
         }
 
-        Arrays.sort(compArr, Comparator.comparingInt(a -> a[0]));
+        Arrays.sort(sortingArr);
 
-        int compNum = 0;
-        arr[compArr[0][1]] = compNum;
+        List<Integer> sortedList = new ArrayList<>();
+        sortedList.add(sortingArr[0]);
         for (int i = 1; i < n; i++) {
-            if (compArr[i][0] != compArr[i - 1][0]) {
-                compNum++;
+            if (sortedList.get(sortedList.size() - 1) != sortingArr[i]) {
+                sortedList.add(sortingArr[i]);
             }
-
-            arr[compArr[i][1]] = compNum;
         }
 
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < n; i++) {
-            sb.append(arr[i]).append(" ");
+            int start = 0;
+            int end = sortedList.size() - 1;
+
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                if (arr[i] > sortedList.get(mid)) {
+                    start = mid + 1;
+                } else if (arr[i] < sortedList.get(mid)) {
+                    end = mid - 1;
+                } else {
+                    sb.append(mid).append(" ");
+                    break;
+                }
+            }
         }
 
         System.out.print(sb);
