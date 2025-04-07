ee12ce534158ec2b6957ab385054c6fa8881fd5e
종성 <eello.bdev@gmail.com>
2025-04-07 12:04:49 +0900

    [Gold III] Title: 다리 만들기, Time: 200 ms, Memory: 77036 KB -BaekjoonHub

--- eello/백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ eello/백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -1,103 +1,135 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+/**
+ * G3
+ * 2146 - 다리 만들기
+ * https://www.acmicpc.net/problem/2146
+ */
 public class Main {
 
-    private static final int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    private static class Pos {
+        int y;
+        int x;
 
+        public Pos(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+
+    private static final int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     private static int n;
     private static int[][] map;
-    private static List<List<int[]>> coastline = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         map = new int[n][n];
 
-        for (int r = 0; r < n; r++) {
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int c = 0; c < n; c++) {
-                map[r][c] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        boolean[][] visit = new boolean[n][n];
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                if (map[r][c] == 0 || visit[r][c]) {
-                    continue;
+        int island = 2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] == 1) {
+                    bfs(new Pos(i, j), island++);
                 }
-
-                bfs(r, c, visit);
             }
         }
 
         int answer = Integer.MAX_VALUE;
-        int area = coastline.size();
-        for (int i = 0; i < area; i++) {
-            for (int j = i + 1; j < area; j++) {
-                answer = Math.min(answer, getMinLength(coastline.get(i), coastline.get(j)));
-            }
-        }
-
-        System.out.print(answer);
-    }
-
-    private static int getMinLength(List<int[]> cl1, List<int[]> cl2) {
-        int min = Integer.MAX_VALUE;
-        for (int[] a : cl1) {
-            for (int[] b : cl2) {
-                min = Math.min(min, Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]) - 1);
-            }
-        }
-
-        return min;
-    }
-
-    private static void bfs(int sr, int sc, boolean[][] visit) {
-        Queue<int[]> que = new ArrayDeque<>();
-        que.add(new int[]{sr, sc});
-        visit[sr][sc] = true;
-
-        List<int[]> cl = new ArrayList<>();
-
-        while (!que.isEmpty()) {
-            int[] cur = que.poll();
-            int r = cur[0];
-            int c = cur[1];
-
-            if (isCoastline(r, c)) {
-                cl.add(cur);
-            }
-
-            for (int[] d : dir) {
-                int nr = r + d[0];
-                int nc = c + d[1];
-
-                if (checkRange(nr, nc) && map[nr][nc] == 1 && !visit[nr][nc]) {
-                    visit[nr][nc] = true;
-                    que.add(new int[]{nr, nc});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (1 < map[i][j] && map[i][j] < island - 1 && isEdge(i, j)) {
+                    answer = Math.min(answer, getBridgeLength(new Pos(i, j)));
                 }
             }
         }
 
-        coastline.add(cl);
+        System.out.println(answer);
     }
 
-    private static boolean checkRange(int r, int c) {
-        return 0 <= r && r < n && 0 <= c && c < n;
-    }
-
-    private static boolean isCoastline(int r, int c) {
+    private static boolean isEdge(int y, int x) {
         for (int[] d : dir) {
-            int nr = r + d[0];
-            int nc = c + d[1];
+            int ny = y + d[0];
+            int nx = x + d[1];
 
-            if (checkRange(nr, nc) && map[nr][nc] == 0) {
+            if (!checkRange(ny, nx))
+                continue;
+
+            if (map[ny][nx] == 0)
                 return true;
-            }
         }
 
         return false;
+    }
+
+    private static void bfs(Pos start, int island) {
+        Queue<Pos> q = new LinkedList<>();
+
+        q.offer(start);
+        map[start.y][start.x] = island;
+
+        while (!q.isEmpty()) {
+            Pos now = q.poll();
+
+            for (int[] d : dir) {
+                int ny = now.y + d[0];
+                int nx = now.x + d[1];
+
+                if (checkRange(ny, nx) && map[ny][nx] == 1) {
+                    map[ny][nx] = island;
+                    q.offer(new Pos(ny, nx));
+                }
+            }
+        }
+    }
+
+    private static int getBridgeLength(Pos start) {
+        Queue<Pos> q = new LinkedList<>();
+        boolean[][] v = new boolean[n][n];
+
+        q.offer(start);
+        v[start.y][start.x] = true;
+        int island = map[start.y][start.x];
+
+        int length = -1;
+        while (!q.isEmpty()) {
+            length++;
+            int qSize = q.size();
+            for (int i = 0; i < qSize; i++) {
+                Pos now = q.poll();
+
+                for (int[] d : dir) {
+                    int ny = now.y + d[0];
+                    int nx = now.x + d[1];
+
+                    if (checkRange(ny, nx) && map[ny][nx] != island && !v[ny][nx]) {
+                        if (map[ny][nx] != 0) {
+                            return length;
+                        }
+
+                        v[ny][nx] = true;
+                        q.offer(new Pos(ny, nx));
+                    }
+                }
+            }
+        }
+
+        return Integer.MAX_VALUE;
+    }
+
+    private static boolean checkRange(int y, int x) {
+        return 0 <= y && y < n && 0 <= x && x < n;
     }
 }
