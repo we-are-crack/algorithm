6642f940c10e7e63c736a3938951d7f9bcf0ae25
on1ystar <tjdwls0607@naver.com>
2025-04-13 16:04:13 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- on1ystar/백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ on1ystar/백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,20 +1,23 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.IOException;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] d = new int[n + 1][2];
-        int stair = Integer.parseInt(br.readLine());
-        d[1] = new int[]{stair, stair};
-        for(int i = 2; i <= n; i++) {
-            stair = Integer.parseInt(br.readLine());
-            d[i][0] = stair + d[i - 1][1];
-            d[i][1] = stair + Math.max(d[i - 2][0], d[i - 2][1]);
+
+        int[][] stairs = new int[n + 1][2];
+        stairs[1][0] = Integer.parseInt(br.readLine());
+        stairs[1][1] = stairs[1][0];
+        for (int i = 2; i <= n; i++) {
+            int value = Integer.parseInt(br.readLine());
+            stairs[i][0] = stairs[i - 1][1] + value;
+            stairs[i][1] = Math.max(stairs[i - 2][0], stairs[i - 2][1]) + value;
         }
-        System.out.println(Math.max(d[n][0], d[n][1]));
+
+        System.out.println(Math.max(stairs[n][0], stairs[n][1]));
     }
 }
