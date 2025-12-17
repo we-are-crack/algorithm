42b3959029eaa48e4f995ec0c4024e60212b6ef9
종성 <eello.bdev@gmail.com>
2025-12-17 12:40:13 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 1.05 ms, Memory: 83 MB -BaekjoonHub

--- eello/프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ eello/프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -8,45 +8,45 @@ class Solution {
         → n개의 원소가 있는 배열에 대한 공약수 집합을 구해야 함
             1. 각 배열의 최소값 원소에 대한 약수들 구함: a
             2. a를 사용해서 해당 배열에 대한 최대 공약수를 구함: gcd
-            3. gcd의 약수 집합이 해당 배열에 대한 전체 공약수 집합
+            3. gcd의 약수 집합이 해당 배열에 대한 공약수 집합
             
         각 배열에 대한 공약수 집합의 원소 중 겹치지 않는 원소의 최대값부터 반대편 원소를 나눌 수 있는지 없는지 검사
     */
     
     public int solution(int[] arrayA, int[] arrayB) {
-        int[][] array = new int[2][];
-        array[0] = arrayA;
-        array[1] = arrayB;
+        int gcdA = getGcd(arrayA);
+        int gcdB = getGcd(arrayB);
         
-        List<Integer> cdA = getCommonDivisors(arrayA);
-        List<Integer> cdB = getCommonDivisors(arrayB);
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        for (Integer cda : cdA) {
-            pq.add(new int[] {cda, 1});
-        }
-        
-        for (Integer cdb : cdB) {
-            pq.add(new int[] {cdb, 0});
-        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.addAll(getDivisors(gcdA));
+        pq.addAll(getDivisors(gcdB));
         
         int answer = 0;
         while (!pq.isEmpty()) {
-            int[] divisor = pq.poll();
+            int divisor = pq.poll();
             
-            int div = divisor[0];
-            int[] targetArray = array[divisor[1]];
+            int[] targetArray; // divisor로 나눌 수 있는 숫자가 있는지 검사해야할 배열
+            if (gcdA % divisor == 0 && gcdB % divisor == 0) {
+                // 두 배열 모두에 대한 약수인 경우(겹치는 약수인 경우)
+                continue;
+            } else if (gcdA % divisor == 0) {
+                // arrayA에 대한 약수이므로 arrayB에 대해 나눌 수 있는 숫자가 있는지 검사
+                targetArray = arrayB;
+            } else {
+                // arrayB에 대한 약수이므로 arrayA에 대해 나눌 수 있는 숫자가 있는지 검사
+                targetArray = arrayA;
+            }
             
             boolean flag = true;
             for (int elem : targetArray) {
-                if (elem % div == 0) {
+                if (elem % divisor == 0) {
                     flag = false;
                     break;
                 }
             }
             
             if (flag) {
-                answer = div;
+                answer = divisor;
                 break;
             }
         }
@@ -54,16 +54,7 @@ class Solution {
         return answer;
     }
     
-    private boolean isCommonDivisor(int[] array, int divisor) {
-        for (int elem : array) {
-            if (elem % divisor != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    private List<Integer> getCommonDivisors(int[] array) {
+    private int getGcd(int[] array) {
         // 배열의 최소 원소의 약수를 구함
         Arrays.sort(array);
         int min = array[0];
@@ -72,27 +63,25 @@ class Solution {
         
         // 배열의 최소 원소의 약수로 해당 배열의 최대 공약수를 구함
         int gcd = 1;
-        for (Integer div : divisors) {
-            gcd = div;
-            
+        for (Integer div : divisors) {           
             boolean flag = true;
             for (int elem : array) {
-                if (elem % gcd != 0) {
+                if (elem % div != 0) {
                     flag = false;
                     break;
                 }
             }
             
             if (flag) {
+                gcd = div;
                 break;
             }
         }
         
-        // 최대 공약수의 약수가 배열의 공약수 집합
-        List<Integer> commonDivisors = getDivisors(gcd);
-        return commonDivisors;
+        return gcd;
     }
     
+    // 약수 배열을 오름차순으로 리턴
     private List<Integer> getDivisors(int number) {
         List<Integer> divisors = new ArrayList<>();
         
