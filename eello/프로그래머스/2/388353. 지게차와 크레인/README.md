545ee0c10dc207bc5357089dd9d881f7ecdbce92
종성 <eello.bdev@gmail.com>
2025-12-24 11:24:48 +0900

    [level 2] Title: 지게차와 크레인, Time: 10.91 ms, Memory: 86.1 MB -BaekjoonHub

--- eello/프로그래머스/2/388353. 지게차와 크레인/README.md
+++ eello/프로그래머스/2/388353. 지게차와 크레인/README.md
@@ -1,10 +1,10 @@
 # [level 2] 지게차와 크레인 - 388353 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388353?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388353) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 24.55 ms
+메모리: 86.1 MB, 시간: 10.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 17일 16:25:48
+2025년 12월 24일 11:24:45
 
 ### 문제 설명
 
--- eello/프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ eello/프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -2,122 +2,120 @@ import java.util.*;
 
 class Solution {
     
-    private final int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    /*
+        storage를 감싸는 패딩을 추가 - 패딩은 모두 빈 공간, 패딩 사이즈 = 2
+        ex) n = 2, m = 2일 때 만들어지는 storage
+        0 0 0 0
+        0 A B 0
+        0 C D 0
+        0 0 0 0
+        
+        패딩 공간은 빈 공간으로 창고 외부
+        → (0, 0)부터 시작해 빈 공간으로 이동해 접근할 수 있는 경우는 창고 외부와 연결된 컨테이너
+            = 지게차로 출고 가능한 컨테이너
+        
+        지게차로 출고
+            - (0, 0)은 추가된 패딩으로 무조건 빈 공간(창고 외부)
+            - (0, 0)부터 시작해 BFS를 돌며 접근할 수 있는 요청된 종류의 모든 컨테이너 출고
+            - 출고된 컨테이너 수 리턴
+            - 출고된 위치는 빈 공간으로 전환
+            - BFS 한 번으로 창고 외부와 연결돼 지게차로 접근 가능한 모든 컨테이너 출고 가능
+        
+        크레인으로 출고
+            - 이중 반복문으로 간단하게 처리가능
+            - 출고된 컨테이너 수 리턴
+            - 출고된 위치는 빈 공간으로 전환
+    */
     
-    private int n, m;
-    private String[] storage;
-    private boolean[][] accessible, removed;
+    private static final int PADDING_SIZE = 2;
+    private static final char EMPTY = '.';
+    
+    private int r, c;
+    private char[][] storage;
     
     public int solution(String[] storage, String[] requests) {
         init(storage);
         
-        int answer = n * m;
-        for (String req : requests) {
-            if (req.length() == 1) { // 지게차
-                answer -= removeByLift(req.charAt(0));
-            } else { // 크레인
-                Set<Character> targets = new HashSet<>();
-                for (char ch : req.toCharArray()) {
-                    targets.add(ch);
-                }
-                
-                answer -= removeByCrane(targets);
-            }
-            
-            updateAccessible();
+        int answer = storage.length * storage[0].length();
+        int delivery = 0;
+        for (String request : requests) {
+            char containerType = request.charAt(0);
+            answer -= request.length() == 1 ? 
+                deliveryByForklift(containerType) : deliveryByCrane(containerType);
         }
         
         return answer;
     }
     
     private void init(String[] storage) {
-        n = storage.length;
-        m = storage[0].length();
+        r = storage.length + PADDING_SIZE;
+        c = storage[0].length() + PADDING_SIZE;
+        this.storage = new char[r][c];
         
-        this.storage = storage;
+        for (int y = 0 ; y < r; y++) {
+            Arrays.fill(this.storage[y], EMPTY);
+        }
         
-        accessible = new boolean[n][m];
-        removed = new boolean[n][m];
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
-                    accessible[i][j] = true;
-                }
+        for (int y = 1; y < r - 1; y++) {
+            for (int x = 1; x < c - 1; x++) {
+                this.storage[y][x] = storage[y - 1].charAt(x - 1);
             }
         }
     }
     
-    private int removeByLift(char target) { // 지게차로 물품 꺼냄
-        int rm = 0;
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < m; c++) {
-                if (removed[r][c] || storage[r].charAt(c) != target || !accessible[r][c]) {
-                    // 삭제되었거나, 타겟이 아니거나, 접근이 불가능하면
-                    continue;
-                }
-
-                rm++;
-                removed[r][c] = true;
-            }
-        }
+    // 지게차로 접근할 수 있는 컨테이너 중 containerType에 해당하는 컨테이너를 출고
+    // 출고한 컨테이너 수 리턴
+    private int deliveryByForklift(char containerType) {
+        int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        int delivery = 0; // 출고된 컨테이너 수
         
-        return rm;
-    }
-    
-    private int removeByCrane(Set<Character> targets) { // 크레인으로 물품 꺼냄
-        int rm = 0;
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < m; c++) {
-                if (removed[r][c] || !targets.contains(storage[r].charAt(c))) {
-                    // 삭제되었거나, 타겟이 아니면
-                    continue;
-                }
-                
-                rm++;
-                removed[r][c] = true;
-            }
-        }
-                
-        return rm;
-    }
-    
-    private void updateAccessible() {
-        Queue<int[]> q = new ArrayDeque<>();
-        boolean[][] visit = new boolean[n][m];
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < m; c++) {
-                if (removed[r][c] && accessible[r][c]) {
-                    q.offer(new int[] {r, c});
-                    visit[r][c] = true;
-                }
-            }
-        }
+        Queue<int[]> que = new ArrayDeque<>();
+        boolean[][] visit = new boolean[r][c];
         
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int r = cur[0];
-            int c = cur[1];
+        que.add(new int[] {0, 0});
+        visit[0][0] = true;
+        
+        while (!que.isEmpty()) {
+            int[] cur = que.poll();
             
             for (int[] d : dir) {
-                int nr = r + d[0];
-                int nc = c + d[1];
+                int nr = cur[0] + d[0];
+                int nc = cur[1] + d[1];
                 
-                if (!checkRange(nr, nc) || visit[nr][nc]) {
-                    continue;
-                }
-                
-                if (removed[nr][nc]) {
-                    q.offer(new int[] {nr, nc});
-                    visit[nr][nc] = true;
-                } else {
-                    accessible[nr][nc] = true;
+                if (isValidRange(nr, nc) && !visit[nr][nc]) {
+                    if (storage[nr][nc] == EMPTY) {
+                        que.add(new int[] {nr, nc});
+                        visit[nr][nc] = true;
+                    } else if (storage[nr][nc] == containerType) {
+                        storage[nr][nc] = EMPTY;
+                        delivery += 1;
+                        visit[nr][nc] = true;
+                    }
                 }
             }
         }
+        
+        return delivery;
     }
     
-    private boolean checkRange(int r, int c) {
-        return 0 <= r && r < n && 0 <= c && c < m;
+    // 크레인로 접근할 수 있는 컨테이너 중 containerType에 해당하는 컨테이너를 출고
+    // 출고한 컨테이너 수 리턴
+    private int deliveryByCrane(char containerType) {
+        int delivery = 0;
+        
+        for (int y = 1; y < r - 1; y++) {
+            for (int x = 1; x < c - 1; x++) {
+                if (storage[y][x] == containerType) {
+                    storage[y][x] = EMPTY;;
+                    delivery += 1;
+                }
+            }
+        }
+        
+        return delivery;
+    }
+    
+    private boolean isValidRange(int y, int x) {
+        return 0 <= y && y < r && 0 <= x && x < c;
     }
 }
