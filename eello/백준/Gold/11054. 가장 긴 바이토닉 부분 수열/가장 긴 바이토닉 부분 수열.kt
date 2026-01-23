067c82c82a88102ce98533d257af742bff568357
종성 <eello.bdev@gmail.com>
2026-01-23 15:05:01 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 148 ms, Memory: 23272 KB -BaekjoonHub

--- eello/백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.kt
+++ eello/백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.kt
@@ -1,5 +1,3 @@
-import kotlin.math.max
-
 fun main() {
     val n = readln().toInt()
     val arr = readln().split(" ").map(String::toInt)
@@ -7,18 +5,26 @@ fun main() {
     val inc = IntArray(n) { 1 } // 정방향로 증가하는 LIS 길이
     val dec = IntArray(n) { 1 } // 역방향로 증가하는 LIS 길이
 
-    for (i in 0 until n) {
-        for (j in 0 until i) { // 정방향 LIS 계산
-            if (arr[i] > arr[j]) {
-                inc[i] = max(inc[i], inc[j] + 1)
-            }
-        }
+    val lis = mutableListOf<Int>()
+    for (i in 0 until n) { // 정방향 LIS 계산
+        var insertPoint = lis.binarySearch(arr[i])
+        if (insertPoint < 0) insertPoint = -(insertPoint + 1)
 
-        for (j in n - i until n) { // 역방향 LIS 계산
-            if (arr[n - 1 - i] > arr[j]) {
-                dec[n - 1 - i] = max(dec[n - 1 - i], dec[j] + 1)
-            }
-        }
+        if (insertPoint == lis.size) lis.add(arr[i])
+        else lis[insertPoint] = arr[i]
+
+        inc[i] = insertPoint + 1
+    }
+
+    lis.clear()
+    for (i in n - 1 downTo 0) { // 역방향 LIS 계산
+        var insertPoint = lis.binarySearch(arr[i])
+        if (insertPoint < 0) insertPoint = -(insertPoint + 1)
+
+        if (insertPoint == lis.size) lis.add(arr[i])
+        else lis[insertPoint] = arr[i]
+
+        dec[i] = insertPoint + 1
     }
 
     val answer = arr.indices.maxOf { i -> inc[i] + dec[i] - 1 }
