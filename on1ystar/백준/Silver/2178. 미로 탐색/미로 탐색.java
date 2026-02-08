2b0a3393478536cf2e9a0996291746c02fcb3e93
on1ystar <tjdwls0607@naver.com>
2026-02-08 17:04:38 +0900

    [Silver I] Title: 미로 탐색, Time: 104 ms, Memory: 14440 KB -BaekjoonHub

--- on1ystar/백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ on1ystar/백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,51 +1,76 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    private static int n, m;
     private static final int[] dx = {1, 0, -1, 0};
     private static final int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] maze = new int[n][m];
 
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int[][] maze = new int[n][m];
         for (int i = 0; i < n; i++) {
-            String line = br.readLine();
+            String s = br.readLine();
+
             for (int j = 0; j < m; j++) {
-                maze[i][j] = line.charAt(j) - '0';
+                maze[i][j] = s.charAt(j) - '0';
             }
         }
 
-        solve(maze, n, m);
-    }
-
-    private static void solve(int[][] maze, int n, int m) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
+        Deque<Pos> q = new ArrayDeque<>();
+        q.offer(new Pos(0, 0, 1));
+        maze[0][0] = 0;
         while (!q.isEmpty()) {
-            int[] nowPos = q.poll();
-            if (nowPos[0] == n - 1 && nowPos[1] == m - 1) {
-                break;
+            Pos pos = q.poll();
+
+            if (pos.getX() == n - 1 && pos.getY() == m - 1) {
+                System.out.println(pos.getPath());
+                return;
             }
 
             for (int i = 0; i < 4; i++) {
-                int[] nextPos = new int[] {nowPos[0] + dy[i], nowPos[1] + dx[i]};
-                if (nextPos[0] < 0 || nextPos[0] >= n || nextPos[1] < 0 || nextPos[1] >= m) { continue; }
-                if (maze[nextPos[0]][nextPos[1]] == 1) {
-                    maze[nextPos[0]][nextPos[1]] = maze[nowPos[0]][nowPos[1]] + 1;
-                    q.offer(new int[]{nextPos[0], nextPos[1]});
-                }
+                int nx = pos.getX() + dx[i];
+                int ny = pos.getY() + dy[i];
+
+                if (!valid(nx, ny) || maze[nx][ny] == 0) continue;
+
+                q.offer(new Pos(nx, ny, pos.getPath() + 1));
+                maze[nx][ny] = 0;
             }
         }
 
-        System.out.println(maze[n - 1][m - 1]);
+    }
+
+    private static boolean valid(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < m;
+    }
+
+    private static class Pos {
+        private final int x;
+        private final int y;
+        private final int path;
+
+        public Pos(int x, int y, int path) {
+            this.x = x;
+            this.y = y;
+            this.path = path;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public int getPath() {
+            return path;
+        }
     }
 }
