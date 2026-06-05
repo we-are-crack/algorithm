078333484c78f9c625dc4d77b2144fbaefad1ac3
종성 <eello.developer@gmail.com>
2026-06-05 18:12:37 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: , Memory: undefined -BaekjoonHub

--- eello/프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ eello/프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,14 +1,18 @@
 with joined_at_2021 as (
     select * from user_info
-    where year(joined) = '2021'
+    where joined >= '2021-01-01' and joined < '2022-01-01'
+),
+total_user_count as (
+    select count(*) as total_cnt from joined_at_2021
 )
 
 select
-    year(sales_date) as year,
-    month(sales_date) as month,
+    year(os.sales_date) as year,
+    month(os.sales_date) as month,
     count(distinct u.user_id) as purchased_users,
-    round(count(distinct u.user_id) / (select count(*) from joined_at_2021), 1) as purchased_ratio
+    round(count(distinct u.user_id) / t.total_cnt, 1) as purchased_ratio
 from joined_at_2021 u
 join online_sale os using(user_id)
-group by year(sales_date), month(sales_date)
+cross join total_user_count t
+group by year(os.sales_date), month(os.sales_date)
 order by year, month
